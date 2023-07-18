cd35a33ea785db4abc315364ce8378ce6cfa6509
NhloniNKwinika <hloninkwinika@gmail.com>
2023-07-18 21:12:25 +0200

    do while example 2

--- WhileLoop/Program.cs
+++ WhileLoop/Program.cs
@@ -1,7 +1,18 @@
-﻿var number = 0;
-while (number < 10)
+﻿//checks if the value of number is less than 10 using a while loop
+
+//var number = 0;
+//while (number < 10)
+//{
+//    number++;
+//    Console.WriteLine($"The number is {number}");
+//}
+//Console.WriteLine("The loop is finished");
+
+Console.Write("Enter a string: ");
+var inputString = Console.ReadLine();
+while (inputString.Length < 15)
 {
-    number++;
-    Console.WriteLine($"The number is {number}");
+    inputString += 'a';
+    Console.WriteLine(inputString);
 }
 Console.WriteLine("The loop is finished");
