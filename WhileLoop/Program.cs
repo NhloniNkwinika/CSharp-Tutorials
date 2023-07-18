41ef8d2a47a976af4ad6ae79d9c16c404a2cd48f
NhloniNKwinika <hloninkwinika@gmail.com>
2023-07-18 21:28:20 +0200

    Exercise 1

--- WhileLoop/Program.cs
+++ WhileLoop/Program.cs
@@ -1,13 +1,14 @@
 ﻿//checks if the value of number is less than 10 using a while loop
 
-//var number = 0;
-//while (number < 10)
-//{
-//    number++;
-//    Console.WriteLine($"The number is {number}");
-//}
-//Console.WriteLine("The loop is finished");
+var number = 0;
+while (number < 10)
+{
+    number++;
+    Console.WriteLine($"The number is {number}");
+}
+Console.WriteLine("The loop is finished");
 
+//determines the length of a string and add a character
 Console.Write("Enter a string: ");
 var inputString = Console.ReadLine();
 while (inputString.Length < 15)
@@ -16,3 +17,19 @@ while (inputString.Length < 15)
     Console.WriteLine(inputString);
 }
 Console.WriteLine("The loop is finished");
+
+//Question 1:
+/*Using the while loop, implement the CalculateSumOfNumbersBetween method,
+ * which given two integers called firstNumber and lastNumber,
+ * should calculate the sum of numbers between them (including the numbers themselves).*/
+int CalculateSumOfNumbersBetween(int firstNumber, int lastNumber)
+{
+    int currentNumber = firstNumber;
+    int sum = 0;
+    while (currentNumber <= lastNumber)
+    {
+        sum += currentNumber;
+        currentNumber++;
+    }
+    return sum;
+}
