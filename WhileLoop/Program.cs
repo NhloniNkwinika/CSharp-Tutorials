dac1564756baaf58f6833348c6d5226d0608be8c
NhloniNKwinika <hloninkwinika@gmail.com>
2023-07-20 11:55:53 +0200

    updades

--- WhileLoop/Program.cs
+++ WhileLoop/Program.cs
@@ -2,43 +2,41 @@
 
 using System.Diagnostics.Tracing;
 
-//var number = 0;
-//while (number < 10)
-//{
-//    number++;
-//    Console.WriteLine($"The number is {number}");
-//}
-//Console.WriteLine("The loop is finished");
+var number = 0;
+while (number < 10)
+{
+    number++;
+    Console.WriteLine($"The number is {number}");
+}
+Console.WriteLine("The loop is finished");
 
-////determines the length of a string and add a character
-//Console.Write("Enter a string: ");
-//var inputString = Console.ReadLine();
-//while (inputString.Length < 15)
-//{
-//    inputString += 'a';
-//    Console.WriteLine(inputString);
-//}
-//Console.WriteLine("The loop is finished");
+//determines the length of a string and add a character
+Console.Write("Enter a string: ");
+var inputString = Console.ReadLine();
+while (inputString.Length < 15)
+{
+    inputString += 'a';
+    Console.WriteLine(inputString);
+}
+Console.WriteLine("The loop is finished");
 
-////Question 1:
-///*Using the while loop, implement the CalculateSumOfNumbersBetween method,
-// * which given two integers called firstNumber and lastNumber,
-// * should calculate the sum of numbers between them (including the numbers themselves).*/
-//int CalculateSumOfNumbersBetween(int firstNumber, int lastNumber)
-//{
-//    int currentNumber = firstNumber;
-//    int sum = 0;
-//    while (currentNumber <= lastNumber)
-//    {
-//        sum += currentNumber;
-//        currentNumber++;
-//    }
-//    return sum;
-//}
+//Question 1:
+/*Using the while loop, implement the CalculateSumOfNumbersBetween method,
+ * which given two integers called firstNumber and lastNumber,
+ * should calculate the sum of numbers between them (including the numbers themselves).*/
+int CalculateSumOfNumbersBetween(int firstNumber, int lastNumber)
+{
+    int currentNumber = firstNumber;
+    int sum = 0;
+    while (currentNumber <= lastNumber)
+    {
+        sum += currentNumber;
+        currentNumber++;
+    }
+    return sum;
+}
 
 //difference between a while and do while loop
-
-
 string word;
 do
 {
